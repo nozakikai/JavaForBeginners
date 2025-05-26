703cb1cb1c7cc98689f90f0bfb95a0ab02c0a214
MSI\kai08 <zeal24360040@fsgcl.jp>
2025-05-26 10:09:46 +0900

    確認課題3.3

--- src/RectTest.java
+++ src/RectTest.java
@@ -1,6 +1,34 @@
 
 public class RectTest extends MyFrame{
 	public void run() {
-		fillRect(30,50,200,100);
+		setColor(255,0,0);
+		fillRect(30,40,50,100);
+		
+		setColor(0,255,0);
+		fillRect(130,40,50,100);
+		
+		setColor(0,0,255);
+		fillRect(30,200,150,10);
+		
+		setColor(0,0,0);
+		fillRect(90,160,30,30);
+		
+		setColor(255,255,0);
+		fillRect(120,160,30,30);
+		
+		setColor(255,0,255);
+		fillRect(150,160,30,30);
+		
+		setColor(0,255,255);
+		fillRect(180,160,30,30);
+		
+		setColor(0,0,0);
+		fillRect(210,160,30,30);
+		
+		setColor(127,127,127);
+		fillRect(240,160,30,30);
+		
+		setColor(255,255,255);
+		fillRect(40,50,15,15);
 	}
 }
