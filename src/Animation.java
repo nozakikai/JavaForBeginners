4f79ecb3ad556c00e4ba7e4ac5c286eadbb83c1a
MSI\kai08 <zeal24360040@fsgcl.jp>
2025-06-02 11:47:44 +0900

    確認問題6.2

--- src/Animation.java
+++ src/Animation.java
@@ -1,15 +1,37 @@
 
-public class Animation extends MyFrame{
+public class Animation extends MyFrame {
 	public void run() {
-		int x=30;
-		while(x<=170) {
+		int x = 10;
+		int y = 70;
+		
+		while (y <= 250) { //永久に繰り返す
 			//(1)画面を消す
 			clear();
 			//(2)四角を表示する
-		setColor(0,128,0);
-		fillOval(x,80,70,70);
-		x+=4;
-		sleep(0.01);
+			setColor(0, 128, 0);
+			fillRect(x, y, 10, 100);
+			x+= 5;
+			y += 5;
+			sleep(0.01);
+		}
+		while (x >= 10) { //永久に繰り返す
+			//(1)画面を消す
+			clear();
+			//(2)四角を表示する
+			setColor(0, 128, 0);
+			fillRect(x, y, 10, 100);
+			x -= 5;
+			sleep(0.01);
+		}
+		while (y >= 50) { //永久に繰り返す
+			//(1)画面を消す
+			clear();
+			//(2)四角を表示する
+			setColor(0, 128, 0);
+			fillRect(x, y, 10, 100);
+			x+= 5;
+			y -= 5;
+			sleep(0.01);
 		}
 	}
 
---
+++ src/DisplayFaceObject.java
@@ -0,0 +1,14 @@
+
+public class DisplayFaceObject extends MyFrame{
+	public void run() {
+		Face face1 =new Face(30,30);
+		face1.draw(this);
+		
+		Face face2 =new Face(200,200);
+		face2.draw(this);
+		
+		Face face3 =new Face(200,30);
+		face3.draw(this);
+	}
+
+}
