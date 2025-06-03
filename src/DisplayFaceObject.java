d8574ac04bb22aecbf7c74b00691eda0dd4611bc
MSI\kai08 <zeal24360040@fsgcl.jp>
2025-06-03 10:42:54 +0900

    練習問題6-1と6-2

--- src/DisplayFaceObject.java
+++ src/DisplayFaceObject.java
@@ -1,14 +1,23 @@
 
-public class DisplayFaceObject extends MyFrame{
+public class DisplayFaceObject extends MyFrame {
 	public void run() {
-		Face face1 =new Face(30,30);
+		Face face1 = new Face(30, 30, 10, 5);
 		face1.draw(this);
-		
-		Face face2 =new Face(200,200);
+
+		Face face2 = new Face(200, 200, -10, -5);
 		face2.draw(this);
-		
-		Face face3 =new Face(200,30);
-		face3.draw(this);
+
+		for (int i = 0; i < 30; i++) {
+			clear();
+			face1.draw(this);
+			face2.draw(this);
+
+			face1.move();
+			face2.move();
+
+			sleep(0.1);
+
+		}
 	}
 
 }
